32616427aba9a814fbd314070ad655c5a861e212
DESKTOP-NULLMQQ\imdg0 <limtg2@daum.net>
2021-04-04 23:23:07 +0900

    add comment

--- src/main/java/com/example/demo/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		/**
+		 * test 
+		 * test
+		 * awtest
+		 * 
+		 */
 	}
 
 }
